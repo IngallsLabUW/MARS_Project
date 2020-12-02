0ed759bda52dbaac80f47abedadb2489cf5da174
Regina Lionheart <regina@lionhe.art>
2020-12-02 10:27:00 -0800

    Import Metlin matching file from WKumler.

---
+++ Metlin_Matching.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+source("Functions.R")
+source("KRH_Functions.R")
+
+
+file.pattern <- "bet_"
+
+filenames <- RemoveCsv(list.files(path = "data_from_lab_members/", pattern = file.pattern))
+filepath <- file.path("data_from_lab_members", paste(filenames, ".csv", sep = ""))
+
+for (i in filenames) {
+  filepath <- file.path("data_from_lab_members/", paste(i, ".csv", sep = ""))
+  assign(make.names(i), read.csv(filepath, stringsAsFactors = FALSE))
+}
