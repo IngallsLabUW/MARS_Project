307155f13e11bb313e4cef2407a2efc49cb5ca0a
Regina Lionheart <regina@lionhe.art>
2020-12-07 15:28:09 -0800

    Combine temp MoNA file with original script.

--- MoNA_Matching.R
+++ MoNA_Matching.R
@@ -4,14 +4,48 @@ library(tidyverse)
 source("Functions.R")
 source("KRH_Functions.R")
 
-# TODO
-# Need to filter out the standards from this "check". No point in assigning things we already have!
 
-# All code originally from the massbankMS2MatchNEGATIVE function. Edited by RML.
+### Takes forever. Parallelization is not working right now.
+### Creating many duplicate rows. Join explosion is happening.
+MSMScosine1_df <- function(df) {
+  scan1 <- scantable(df["scan1"])
+  scan2 <- scantable(df["scan2"])
+  mass1 <- df["mass1"]
+  mass2 <- df["mass2"]
+  print("scan1")
+  print(scan1)
+  print("scan2")
+  print(scan2)
+  
+  mztolerance<-0.02
+  
+  w1<-(scan1[,1]^2)*sqrt(scan1[,2])
+  w2<-(scan2[,1]^2)*sqrt(scan2[,2])
+  
+  diffmatrix <- sapply(scan1[, 1], function(x) scan2[, 1] - x)
+  print("diffmatrix")
+  print(diffmatrix)
+  sameindex <- which(abs(diffmatrix)<mztolerance,arr.ind=T)
+  
+  similarity<-sum(w1[sameindex[,2]]*w2[sameindex[,1]])/(sqrt(sum(w2^2))*sqrt(sum(w1^2)))
+  
+  return(similarity)
+}
+
+scantable <- function(Scan) {
+  Try <- read.table(text=as.character(Scan),
+                    col.names = c("mz","intensity"), fill = TRUE) %>% 
+    mutate(mz = as.numeric(mz %>% str_replace(",", "")),
+           intensity = as.numeric(intensity %>% str_replace(";", ""))) %>%
+    mutate(intensity = round(intensity/max(intensity)*100, digits = 1)) %>%
+    filter(intensity > 0.5) %>%
+    arrange(desc(intensity))
+  
+  return(Try)
+}  
+
 Cosine.Score.Cutoff <- 0.5 
 MassBank.ppm.Cutoff <- 5
-
-# Imports -----------------------------------------------------------------
 
 # One of four relational spreadsheets from the MoNA download
 MoNA.Spectra <- read.csv("data_extra/NEG_Spectra.csv") 
@@ -27,11 +61,19 @@ MS2 <- as.character(Experimental.Spectra["MS2"])
 MF.Fraction <- as.data.frame(Experimental.Spectra["MF.Fraction"]) 
 
 Experimental.Spectra.ForJoin <- Experimental.Spectra %>% 
-  rename(MH_mass = mz) 
+  rename(MH_mass = mz) %>%
+  mutate(No.MS2 = ifelse(is.na(MS2), TRUE, FALSE)) %>%
+  filter(No.MS2 == FALSE) %>%
+  select(-No.MS2)
 
 # Subtract hydrogen for reference database
 MoNA.Spectra.MHMass <- MoNA.Spectra %>%
-  mutate(MH_mass = M_mass - 1.0072766) 
+  mutate(MH_mass = M_mass - 1.0072766) %>%
+  select(ID, Names, spectrum_KRHform_filtered, MH_mass) 
+MoNA.Spectra.MHMass[MoNA.Spectra.MHMass==""]<-NA
+MoNA.Spectra.MHMass <- MoNA.Spectra.MHMass %>%
+  drop_na()
+
 
 ## Example masses for testing
 testmass1 <- 116.07093 
@@ -46,67 +88,49 @@ testdf2 <- MoNA.Spectra.MHMass %>%
 testdf <- testdf1 %>% rbind(testdf2) %>%
   arrange(MH_mass)
 
-## Dataframe up to 500 rows for quick processing
-halftestdf <- MoNA.Spectra.MHMass %>%
-  slice(1:1000)
-  
 ## Full function
-MakeCandidates <- function(testmass) {
+MakeCandidates <- function(MoNA.Mass) {
   Candidates <- MoNA.Spectra.MHMass %>% 
-    filter(MH_mass > testmass - 0.020,  
-           MH_mass < testmass + 0.020) #%>% 
-    difference_inner_join(Experimental.Spectra.ForJoin, by = "MH_mass", max_dist = 0.02) #%>%  ### SOMETHING HAPPENING HERE
-    # mutate(scan1 = spectrum_KRHform_filtered, # scan1 is MS2 from MoNA 
-    #        scan2 = MS2, # scan2 is MS2 from the experimental data 
-    #        mass1 = MH_mass.x, # mass1 is the primary mass from MoNA 
-    #        mass2 = MH_mass.y) # mass2 is the primary mass from experimental data
+    filter(MH_mass > MoNA.Mass - 0.020,  
+           MH_mass < MoNA.Mass + 0.020) %>% 
+    difference_inner_join(Experimental.Spectra.ForJoin, by = "MH_mass", max_dist = 0.02) %>% 
+    mutate(scan1 = spectrum_KRHform_filtered, # scan1 is MS2 from MoNA
+           scan2 = MS2, # scan2 is MS2 from the experimental data
+           mass1 = MH_mass.x, # mass1 is the primary mass from MoNA
+           mass2 = MH_mass.y) # mass2 is the primary mass from experimental data
   
-  # if (length(Candidates$ID) == 0) {
-  #   print("There are no potential candidates.")
-  #   No.Match.Return <- MF.Fraction %>%
-  #     mutate(MassBankMatch = NA, 
-  #            MassBankppm = NA,
-  #            MassBankCosine1 = NA)
-  # 
-  #   return(No.Match.Return)
-  # }
+  if (length(Candidates$ID) == 0) {
+    print("There are no potential candidates.")
+    No.Match.Return <- MF.Fraction %>%
+      mutate(MassBankMatch = NA,
+             MassBankppm = NA,
+             MassBankCosine1 = NA)
+    
+    return(No.Match.Return)
+  }
   
   # Add cosine similarity scores
-  #print("Making potential candidates")
+  print("Making potential candidates")
   
-  #Candidates$Cosine1 <- apply(Candidates, 1, FUN=function(x) MSMScosine1_df(x)) # this is being made into a list. why
+  Candidates$Cosine1 <- apply(Candidates, 1, FUN=function(x) MSMScosine1_df(x)) 
   
-  # Candidates.Filtered.Cosine <- Candidates %>%
-  #   #filter(Cosine1 > Cosine.Score.Cutoff) %>%
-  #   arrange(desc(Cosine1))
-  # 
-  # Final.Candidates <- Candidates.Filtered.Cosine %>%
-  #   mutate(MassBankMatch = paste(Names, ID, sep = " ID:"),
-  #          MassBankppm = abs(mass1 - mass2) / mass2 * 10^6,
-  #          MassBankCosine1 = Cosine1) %>%
-  #   unique() %>%
-  #   #filter(MassBankppm < MassBank.ppm.Cutoff) %>% # In the current data, this step removes everything.
-  #   select(MF.Fraction, MassBankMatch:MassBankCosine1)
+  Candidates.Filtered.Cosine <- Candidates %>%
+    filter(Cosine1 > Cosine.Score.Cutoff) %>%
+    arrange(desc(Cosine1))
   
-  #return(Final.Candidates)
-  return(Candidates)
+  Final.Candidates <- Candidates.Filtered.Cosine %>%
+    mutate(MassBankMatch = paste(Names, ID, sep = " ID:"),
+           MassBankppm = abs(mass1 - mass2) / mass2 * 10^6,
+           MassBankCosine1 = Cosine1) %>%
+    unique() %>%
+    filter(MassBankppm < MassBank.ppm.Cutoff) #%>% 
+  #select(MF.Fraction, MassBankMatch:MassBankCosine1)
+  
+  return(Final.Candidates)
 }
 
-halftestoutput <- lapply(unique(halftestdf$MH_mass), MakeCandidates)
-halftestoutputdf <- bind_rows(halftestoutput)
+output <- lapply(unique(MoNA.Spectra.MHMass$MH_mass), MakeCandidates)
+outputdf <- bind_rows(output)
 
-# When everything is fixed ---------------------------------------------
-#output <- lapply(unique(MoNA.Spectra.MHMass$MH_mass), MakeCandidates)
-#outputdf <- bind_rows(output)
-#output <- mclapply(unique(MoNA.Spectra.MHMass$MH_mass), MakeCandidates, detectCores())
+#test <- mclapply(unique(MoNA.Spectra.MHMass$MH_mass), MakeCandidates, detectCores())
 
-# Here’s how I normally implemented the massbankMS2MatchPOSITIVE function I gave you earlier:
-# Make sure whatever you try can be run easily with parallel processing, I think that was why I went with it.
-# Where PosDat is a dataframe with mz and MS2 (in KRH form)
-
-# plyr performs function on row of dataframe. has to be parallelizable. 
-
-# parallel is differnet on mac/pcs. 
-
-# PositiveMBMatches <- adply(PosDat, 1, .fun=function(x) massbankMS2MatchPOSITIVE(x), .parallel = TRUE) %>%
-#   filter(!is.na(MassBankMatch))
