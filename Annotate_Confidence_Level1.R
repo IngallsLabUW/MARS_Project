3294cdca5ec7c6984a778bb6e105fee021d61147
Regina Lionheart <regina@lionhe.art>
2021-02-02 14:17:47 -0800

    Fix 'annotate' spelling in filename.

--- Annotate_Confidence_Level1.R
+++ Annotate_Confidence_Level1.R
@@ -8,68 +8,52 @@ Experimental.Values <- read.csv("data_from_lab_members/MFCluster_Assignments_Kat
 
 # Lab MS2 values
 Cyano.MS2 <- read.csv("data_extra/Cyano_stds_withMS2s.csv") %>%
-  mutate(Column = "Cyano")
+  mutate(Column = "RP",
+         z = NA)
 HILICPos.MS2 <- read.csv("data_extra/HILICPos_stds_withMS2s.csv") %>%
-  mutate(Column = "HILICPos")
+  mutate(Column = "HILIC",
+         z = 1)
 HILICNeg.MS2 <- read.csv("data_extra/HILICNeg_stds_withMS2s.csv") %>%
-  mutate(Column = "HILICNeg")
+  mutate(Column = "HILIC",
+         z = -1)
 
 Standards.MS2 <- Cyano.MS2 %>%
   rbind(HILICPos.MS2, HILICNeg.MS2) %>%
   mutate(rt = rt * 60) %>%
-  rename(rt_ms2 = rt)
-  
-Standards <- read.csv("data_extra/Ingalls_Lab_Standards_Feb1.csv") %>%
-  mutate(rt = (RT..min. * 60)) %>%
-  select(Compound.Name_old, Column, m.z, rt, ionization_form, z)
+  rename(MS2 = MS2s) %>%
+  select(-rtmin, -rtmax, -mz_obs)
 
+Standards <- read.csv("data_extra/Ingalls_Lab_Standards_Feb1.csv") %>%
+  mutate(rt = (RT..min. * 60),
+         m.z = as.numeric(m.z)) %>%
+  rename(mz = m.z,
+         compound = Compound.Name_old) %>%
+  select(compound, Column, rt, ionization_form, z)
 
 ## Confidence Level 1 Matching ----------------------------------------
+Knowns <- Standards %>% # our known standards
+  left_join(Standards.MS2, by = c("compound", "Column", "z")) %>%
+  rename(RT.seconds_standards = rt.x,
+         RT.seconds_MS2s = rt.y)
+
 Unknowns <- Experimental.Values %>% # Experimental
   mutate(Unknown.Compound = paste("Compound_", 1:nrow(Experimental.Values), sep = "")) %>%
-  rename(m.z = mz,
-         KRH.Identification = Identification) %>%
-  select(Unknown.Compound, KRH.Identification, m.z, rt, Column, z, MS2) 
-
-Knowns <- Standards %>% # our known standards
-  rename(compound = Compound.Name_old,
-         mz_known = m.z) %>%
-  left_join(Standards.MS2, by = "compound")
-
-  select(Standards.Compound.Name, m.z, RT.seconds, rt, Column, z.x, z.y, file_name, MS2s)
-
-
-## Temporary test for mz_obs diffs
-# There are no differences larger than 0.02 between mz and mz_obs.
-# There are duplicate entries, from either a missed MS2 or a compound detected in multiple columns.
-# Standards_mz <- Knowns %>% select(Standards.Compound.Name, m.z) %>% unique()
-# MS2_mz <- Cyano.MS2 %>% # no differences larger than 0.02 between mz and mz_obs 
-#   rbind(HILICPos.MS2, HILICNeg.MS2) %>% 
-#   select(1:3, Column) %>% 
-#   unique() %>% # There are duplicates! Sometimes a missed ms2, sometimes detected in both
-#   rename(Standards.Compound.Name = compound) %>%
-#   group_by(Standards.Compound.Name) %>%
-#   add_tally()
-# Difftest <- Standards_mz %>% # 
-#   left_join(MS2_mz) %>%
-#   mutate(diff = abs(m.z - mz)) %>%
-#   filter(diff > 0.02)
-
+  rename(KRH.Identification = Identification) %>%
+  select(Unknown.Compound, KRH.Identification, mz, rt, Column, z, MS2) 
 
 MyFuzzyJoin <- Unknowns %>%
-  difference_inner_join(Knowns, by = c("m.z"), max_dist = 0.02) %>% # needs to be swapped out for variable
-  rename(m.z.Unknowns = m.z.x,
-         Column.Unknowns = Column.x, # can switch these out for variables eventually?
-         z.Unknowns = z.x,
-         RT.seconds.Unknowns = rt.x,
-         m.z.Standards = m.z.y,
-         Column.Standards = Column.y,
-         z.Standards = z.y,
-         RT.seconds.Standards = RT.seconds,
-         MS2.Unknowns = MS2) %>%
-  select(Unknown.Compound, KRH.Identification, Standards.Compound.Name,
-         m.z.Unknowns, m.z.Standards, RT.seconds.Unknowns, RT.seconds.Standards,
-         Column.Unknowns, Column.Standards, z.Unknowns, z.Standards, MS2.Unknowns) 
+  difference_left_join(Knowns, by = c("mz"), max_dist = 0.02) %>% # needs to be swapped out for variable
+  rename(mz_Unknowns = mz.x,
+         Column_Unknowns = Column.x, # can switch these out for variables eventually?
+         z_Unknowns = z.x,
+         RT.seconds_Unknowns = rt,
+         MS2_Unknowns = MS2.x,
+         mz_Standards = mz.y,
+         Column_Standards = Column.y,
+         z_Standards = z.y,
+         MS2_Standards = MS2.y) %>%
+  select(Unknown.Compound, KRH.Identification, compound, mz_Unknowns, mz_Unknowns, RT.seconds_Unknowns, RT.seconds_standards, RT.seconds_MS2s, 
+         Column_Unknowns, Column_Standards, z_Unknowns, z_Standards, MS2_Unknowns, MS2_Standards) 
 
 A1Confidence <- MyFuzzyJoin %>% # mz and 0.02 RT
   filter(z.Unknowns == z.Standards,
