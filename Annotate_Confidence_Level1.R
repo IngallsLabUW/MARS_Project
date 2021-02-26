6d8c62c6d4bd1d058139145f6ffa7a5942afbcd8
Regina Lionheart <regina@lionhe.art>
2021-02-26 14:40:05 -0800

    Add ppm calculation in CL1. Tidy and rename columns in CL1.

--- Annotate_Confidence_Level1.R
+++ Annotate_Confidence_Level1.R
@@ -8,16 +8,8 @@ source("Functions.R")
 mz.flexibility <- 0.02
 rt.flexibility <- 0.02 # seconds
 
-# Unknown (Experimental) Values -----------------------------------------------------
-# Must be in the following format, including capitalization:
-# MassFeature (character), mz (numeric), rt (numeric), column (numeric), z (numeric), MS2 (in concatenated format, character)
-Experimental.Values <- read.csv("data_from_lab_members/MFCluster_Assignments_Katherine.csv") %>%
-  separate(MassFeature_Column, into = c("MassFeature", "drop"), sep = "_") %>%
-  rename(column = Column,
-         KRH_identification = Identification) %>%
-  select(MassFeature, KRH_identification, mz, rt, column, z, MS2)
-
-# Known Values -----------------------------------------------------
+# Theoretical Values ----------------------------------------------------------------------
+# Gather and summarize theoretical values from the standards sheet and the Ingalls MS2 data
 # MS2s
 Cyano.MS2 <- read.csv("data_extra/Standards_MS2/Cyano_stds_withMS2s.csv") %>%
   mutate(column = "RP",
@@ -36,86 +28,99 @@ Standards.MS2 <- Cyano.MS2 %>%
   select(-rtmin, -rtmax, -rt, -mz_obs, -mz)
 
 # Standards csv
-Standards <- read.csv("data_extra/Ingalls_Lab_Standards_Feb1.csv") %>%
+Ingalls.Standards <- read.csv("data_extra/Ingalls_Lab_Standards_Feb1.csv") %>%
   mutate(rt = (RT..min. * 60)) %>%
   rename(compound = Compound.Name_old,
          mz = m.z,
          column = Column) %>%
   select(compound, mz, rt, column, z)
 
-
-# Summarize known and unknown ---------------------------------------------
-# All known variables
-Knowns <- Standards %>% 
+# All theoretical values 
+Theoretical.Values <- Ingalls.Standards %>% 
   left_join(Standards.MS2, by = c("compound", "column", "z")) %>%
   transform(mz = as.numeric(mz))
 
-# All unknown variables
-Unknowns <- Experimental.Values %>% 
-  mutate(compound_unknown = 1:nrow(Experimental.Values)) %>%
-  select(compound_unknown, KRH_identification, mz, rt, column, z, MS2) 
+# Unknown (Experimental) Values -----------------------------------------------------
+# Must be in the following format, including capitalization:
+# MassFeature (character), mz (numeric), rt (numeric), column (numeric), z (numeric), MS2 (in concatenated format, character)
+Experimental.Values <- read.csv("data_from_lab_members/MFCluster_Assignments_Katherine.csv") %>%
+  separate(MassFeature_Column, into = c("MassFeature", "drop"), sep = "_") %>%
+  rename(column = Column,
+         KRH_identification = Identification) %>%
+  mutate(compound_experimental = 1:nrow(.)) %>%
+  select(compound_experimental, KRH_identification, mz, rt, column, z, MS2) 
+
 
 ## Confidence Level 1 Matching ----------------------------------------
-My.Fuzzy.Join <- Knowns %>%
-  difference_left_join(Unknowns, by = c("mz"), max_dist = mz.flexibility) %>% 
-  rename(compound_known = compound,
-         mz_known = mz.x,
-         rt_seconds_known = rt.x,
-         column_known = column.x,
-         z_known = z.x,
-         MS2_known = MS2.x,
-         mz_unknown = mz.y,
-         rt_seconds_unknown = rt.y,
-         column_unknown = column.y,
-         z_unknown = z.y,
-         MS2_unknown = MS2.y) %>%
-  select(compound_unknown, KRH_identification, compound_known, mz_unknown, mz_known, rt_seconds_unknown, rt_seconds_known, 
-         column_unknown, column_known, z_unknown, z_known, MS2_unknown, MS2_known)  %>%
-  arrange(compound_unknown)
+My.Fuzzy.Join <- Theoretical.Values %>%
+  difference_left_join(Experimental.Values, by = c("mz"), max_dist = mz.flexibility) %>% 
+  rename(compound_theoretical = compound,
+         mz_theoretical = mz.x,
+         rt_sec_theoretical = rt.x,
+         column_theoretical = column.x,
+         z_theoretical = z.x,
+         MS2_theoretical = MS2.x,
+         mz_experimental = mz.y,
+         rt_sec_experimental = rt.y,
+         column_experimental = column.y,
+         z_experimental = z.y,
+         MS2_experimental = MS2.y) %>%
+  select(compound_experimental, KRH_identification, compound_theoretical, mz_experimental, mz_theoretical, rt_sec_experimental, rt_sec_theoretical, 
+         column_experimental, column_theoretical, z_experimental, z_theoretical, MS2_experimental, MS2_theoretical)  %>%
+  arrange(compound_experimental)
 
-## Confidence Level 1 ----------------------------------------  
+# Confidence Level 1 ----------------------------------------  
 # DHPS as example
 Confidence.Level.1 <- My.Fuzzy.Join %>%
-  filter(z_unknown == z_known,
-         column_unknown == column_known) %>%
-  mutate(mz_similarity_score = exp(-0.5 * (((mz_unknown - mz_known) / mz.flexibility) ^ 2)),
-         rt_similarity_score = exp(-0.5 * (((rt_seconds_unknown - rt_seconds_known) / rt.flexibility) ^ 2))) %>%
+  filter(z_experimental == z_theoretical,
+         column_experimental == column_theoretical) %>%
+  mutate(mz_similarity_score = exp(-0.5 * (((mz_experimental - mz_theoretical) / mz.flexibility) ^ 2)),
+         rt_similarity_score = exp(-0.5 * (((rt_sec_experimental - rt_sec_theoretical) / rt.flexibility) ^ 2))) %>%
   rowwise() %>%
-  mutate(MS2_cosine_similarity = ifelse(is.na(MS2_unknown) | is.na(MS2_known), 
-                                        NA, MS2CosineSimilarity(MakeScantable(MS2_unknown), MakeScantable(MS2_known)))) %>%
+  mutate(ppm_mass_error = ((abs(mz_experimental - mz_theoretical)) / mz_theoretical) * 10^6) %>%
+  mutate(MS2_cosine_similarity = ifelse(is.na(MS2_experimental) | is.na(MS2_theoretical), 
+                                        NA, MS2CosineSimilarity(MakeScantable(MS2_experimental), MakeScantable(MS2_theoretical)))) %>%
   mutate(total_similarity_score = ifelse(is.na(MS2_cosine_similarity), 
                                          ((mz_similarity_score + rt_similarity_score) / 2) * 100,
                                             ((MS2_cosine_similarity + mz_similarity_score + rt_similarity_score) / 3) * 100))
 
 
-#  Sanity check -------------------------------------------------------------
+# Sanity check -------------------------------------------------------------
 # No fuzzy match (no mz within 0.02 daltons)
-No.Fuzzy.Match <- Unknowns %>%
-  select(compound_unknown) %>%
-  filter(compound_unknown %in% setdiff(1:nrow(Unknowns), My.Fuzzy.Join$compound_unknown)) %>%
+No.Fuzzy.Match <- Experimental.Values %>%
+  select(compound_experimental) %>%
+  filter(compound_experimental %in% setdiff(1:nrow(.), My.Fuzzy.Join$compound_experimental)) %>%
   pull()
 
 # Fuzzy match, but wrong z/column
-No.CL1.Match <- setdiff(1:nrow(Unknowns), sort(c(unique(Confidence.Level.1$compound_unknown), No.Fuzzy.Match)))
+No.CL1.Match <- setdiff(1:nrow(Experimental.Values), 
+                        sort(c(unique(Confidence.Level.1$compound_experimental), 
+                               No.Fuzzy.Match)))
 
-# Have any compounds been lost?
-all.unknowns <- sort(c(No.Fuzzy.Match, No.CL1.Match, unique(Confidence.Level.1$compound_unknown)))
-length(all.unknowns) == length(Unknowns$compound_unknown)
+# Have any compounds been lost? Check for a TRUE output
+all.experimentals <- sort(c(No.Fuzzy.Match, No.CL1.Match, unique(Confidence.Level.1$compound_experimental)))
+length(all.experimentals) == length(Experimental.Values$compound_experimental)
 
+# Make "no match" dataframes for comparison
+No.CL1.Match.df <- Experimental.Values %>%
+  filter(compound_experimental %in% No.CL1.Match)
+No.Fuzzy.Match.df <- Experimental.Values %>%
+  filter(compound_experimental %in% No.Fuzzy.Match)
 
 # Let's land on MARS ------------------------------------------------------
-No.CL1.Match.df <- Unknowns %>%
-  filter(compound_unknown %in% No.CL1.Match)
-No.Fuzzy.Match.df <- Unknowns %>%
-  filter(compound_unknown %in% No.Fuzzy.Match)
-
 Mission.Accomplished <- Confidence.Level.1 %>%
-  mutate(confidence_rank = ifelse(mz_similarity_score == 1 & rt_similarity_score == 1, 1, NA),
-         confidence_source = ifelse(!is.na(confidence_rank), "Ingalls_Standards", NA)) %>%
   bind_rows(No.CL1.Match.df) %>%
   bind_rows(No.Fuzzy.Match.df) %>%
-  arrange(compound_unknown)
+  mutate(confidence_rank = ifelse(mz_similarity_score == 1 & rt_similarity_score == 1 & ppm_mass_error < 7, 1, NA),
+         confidence_source = ifelse(!is.na(confidence_rank), "Ingalls_Standards", NA)) %>%
+  mutate(mz_experimental = ifelse(is.na(mz_experimental) & !is.na(mz), mz, mz_experimental),
+         rt_sec_experimental = ifelse(is.na(rt_sec_experimental) & !is.na(rt), rt, rt_sec_experimental),
+         column_experimental = ifelse(is.na(column_experimental) & !is.na(column), column, column_experimental),
+         z_experimental = ifelse(is.na(z_experimental) & !is.na(z), z, z_experimental),
+         MS2_experimental = ifelse(is.na(MS2_experimental) & !is.na(MS2), MS2, MS2_experimental)) %>%
+  select(-c("mz", "rt", "column", "z", "MS2")) %>%
+  arrange(compound_experimental)
 
 # Save your csv -----------------------------------------------------------
 # Here, save your Mission.Accomplished dataframe for use in the next level.
-
+write.csv(Mission.Accomplished, "data_processed/confidence_level1.csv")
