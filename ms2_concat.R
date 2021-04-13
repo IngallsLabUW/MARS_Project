98d62f90ccc528aea916661340d91754a89d278e
Regina Lionheart <regina@lionhe.art>
2021-04-13 10:41:07 -0700

    Fix MS2 concat issues and add a scaling function.

--- ms2_concat.R
+++ ms2_concat.R
@@ -5,16 +5,17 @@ Cyano.MS2 <- read.csv("data_extra/Standards_MS2/Cyano_stds_withMS2s.csv") %>%
   mutate(column = "RP",
          z = NA)
 
+NonScaled.MS2 <- read.csv("~/work/Ingalls_Standards/MSMS/data_processed/Ingalls_Lab_Standards_MSMS.csv")
 
 # Functions ---------------------------------------------------------------
 ConcatToScan <- function(concat.format) {
-  final <- cSplit(concat.format, "MS2", sep = ";") %>%
+  scantable <- cSplit(concat.format, "MS2", sep = ";") %>%
     pivot_longer(cols = starts_with("MS2")) %>%
     separate(value, sep = ",", into = c("mz", "intensity")) %>%
-    select(compound, mz, intensity) %>%
-    drop_na()
+    select(compound, mz, intensity, file_name) #%>%
+    #drop_na()
   
-  return(final)
+  return(scantable)
 }
 
 ScanToConcat <- function(scantable.format) {
@@ -26,17 +27,28 @@ ScanToConcat <- function(scantable.format) {
     group_by(compound, file_name) %>%
     mutate(MS2 = paste(MS2, collapse = "; ")) %>%
     unique() %>%
-    as.data.frame()
+    as.data.frame() %>%
+    mutate(MS2 = ifelse(MS2 == ("NA, NA"), NA, MS2))
   
   return(concatenated)
 }
 
-# mutate(mz = as.numeric(mz %>% str_replace(",", "")),
-#        intensity = as.numeric(intensity %>% str_replace(";", "")),
-#        intensity = round(intensity / max(intensity) * 100, digits = 1)) %>%
-# filter(intensity > 0.5) %>%
+ScaleMS2 <- function(scan) {
+  scantable <- read.table(text = as.character(scan),
+                          col.names = c("mz", "intensity"), fill = TRUE) %>%
+    mutate(mz = as.numeric(mz %>% str_replace(",", "")),
+           intensity = as.numeric(intensity %>% str_replace(";", "")),
+           intensity = round(intensity / max(intensity) * 100, digits = 1)) %>%
+    filter(intensity > 0.5) %>%
+    arrange(desc(intensity)) %>%
+    unite("MS2", mz:intensity, sep = ", ") %>%
+    mutate(MS2 = paste(MS2, collapse = "; ")) %>%
+    unique()
+  
+  return(scantable)
+}
 
-
+  
 # Both Layouts ------------------------------------------------------------
 Concatenated.Format <- Cyano.MS2 %>%
   rename(MS2 = MS2s) %>%
@@ -46,8 +58,8 @@ Scantable.Format <- cSplit(Concatenated.Format, "MS2", sep = ";") %>%
   group_by(compound, file_name) %>%
   pivot_longer(cols = starts_with("MS2")) %>%
   separate(value, sep = ",", into = c("mz", "intensity")) %>%
-  select(compound, mz, intensity, file_name) #%>%
-#drop_na()
+  select(compound, mz, intensity, file_name)
+
 
 # Switch Between Layouts --------------------------------------------------
 # Scantable -> Concatenated
@@ -58,3 +70,8 @@ Scantable.to.Concatenated <- Scantable.Format %>%
 # Concatenated -> Scantable
 Concatenated.to.Scantable <- Concatenated.Format %>%
   group_modify(~ConcatToScan(.x))
+
+# Scale raw MS2 data --------------------------------------------------
+ScaledforMS2 <- NonScaled.MS2 %>%
+  rowwise() %>%
+  mutate(MS2_2 = ScaleMS2(MS2))
