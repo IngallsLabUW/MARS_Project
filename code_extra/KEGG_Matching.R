0b1a90cfe8f70bb9d97064d678f13d1204ffbd8a
Regina Lionheart <regina@lionhe.art>
2021-04-15 15:57:10 -0700

    First tidying commit of KEGG work.

--- code_extra/KEGG_Matching.R
+++ code_extra/KEGG_Matching.R
@@ -1,16 +1,51 @@
+library(fuzzyjoin)
 library(janitor)
 library(tidyverse)
+
 
 KEGGCompounds_withMasses <- read.csv("data_extra/KEGGCompounds_withMasses.csv", header = TRUE)
 CommonContams <- read.csv("data_extra/CommonContams.csv", header = FALSE) %>%
   slice(-1) %>%
   row_to_names(row = 1) 
 
-MFs.frame <- read.csv("data_processed/confidence_level1.csv") %>%
-  rename(MF_Frac = MF.Fraction) %>%
-  rename(mz = mass2) # temporary rename for initial runthough 
+MFs.frame <- read.csv("data_processed/Example_ConfidenceLevel2.csv")
 
-#Check against the KEGG list of compounds -----
+# Require ppm tolerance, if user doesn't enter it then use 15
+# Figure out 
+# fuzzyjoin to KEGG list
+
+matchedKEGGs <- list()
+keggCompounds <- KEGGCompounds_withMasses %>%
+  rename(Compound = OtherCmpds)
+keggPos <- keggCompounds %>% 
+  select(Compound, PosMZ) %>% 
+  mutate(column = "HILIC",
+         z = 1)
+  mutate(Fraction1 = "HILICPos", Fraction2="CyanoAq", Fraction3= "CyanoDCM") %>% unique() %>% 
+  rename(mz = PosMZ)
+keggCompoundsShort <- keggCompounds %>% select(Compound, NegMZ) %>% 
+  mutate(Fraction1 = "HILICNeg", Fraction2=NA, Fraction3= NA) %>% unique() %>%
+  rename(mz = NegMZ) %>% rbind(keggPos)
+
+# MFstry <- MFs.frame %>% mutate(MF_Frac2 = MF_Frac) %>% separate(MF_Frac2, c("MF", "Frac"), sep =  "_X_") %>% select(-MF) 
+matched <- difference_inner_join(x = keggCompoundsShort, y = MFstry, 
+                                 by = "mz", max_dist = .02, distance_col = NULL) %>%
+  mutate(ppm = (abs(mz.x-mz.y )/mz.x *10^6)) %>%
+  filter(ppm < ppmtol, Frac == Fraction1 | Frac == Fraction2 | Frac == Fraction3) %>%
+  mutate(Frac = as.factor(Frac),mz = mz.x) %>% 
+  select(MF_Frac, Compound, ppm ) %>% rename(Keggppm = ppm)
+
+matchedNames <- left_join(matched, keggCompounds) %>% select(Compound, Name) %>%
+  group_by(Compound) %>%
+  summarise(KEGGMatchesNames = as.character(paste(Name,  collapse=" ")))
+matchedKEGGs[[1]] <- left_join(matched, matchedNames) %>% rename(KeggMatches = Compound)
+matchedKEGGs[[2]] <- matchedKEGGs[[1]] %>%
+  arrange(Keggppm) %>%
+  group_by(MF_Frac) %>%
+  summarise(KeggMatches = as.character(paste(KeggMatches,  collapse="; ")),
+            Keggppm = as.character(paste(Keggppm,  collapse="; ")),
+            KeggNames = as.character(paste(KEGGMatchesNames,  collapse="; ")))
+
 checkKEGG <- function(MFs, ppmtol) { # probably from the output of the MoNA matchings
   if(missing(ppmtol)) {
     ppmtol <- 5
